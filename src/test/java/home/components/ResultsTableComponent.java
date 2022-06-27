57ad55b3689a612f793149b5185a1ce61614844f
Ekaterina_Vasilevskaya <es-vasilevskaya@yandex.ru>
2022-06-27 23:38:57 +0700

    changed directories

---
+++ src/test/java/home/components/ResultsTableComponent.java
@@ -0,0 +1,12 @@
+package home.components;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ResultsTableComponent {
+
+    public void checkResult(String key, String value) {
+        $(".table-responsive").$(byText(key)).parent().shouldHave(text(value));
+    }
+}
---
+++ src/test/java/home/tests/PracticeFormWithPageObjectTests.java
@@ -0,0 +1,71 @@
+package home.tests;
+
+import home.pages.PracticeFormPage;
+import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
+
+public class PracticeFormWithPageObjectTests extends home.tests.TestBaseWithPageObject {
+
+    PracticeFormPage practiceFormPage = new PracticeFormPage();
+
+    @Test
+    void successfulTestPracticeForm() {
+
+        String name = "myName";
+        String lastName = "myLastName";
+        String email = "myEmail@mail.com";
+        String gender = "Female";
+        String mobileNumber = "0123456789";
+        String birthday = "30 August,1992";
+        String dayBirthday = "30";
+        String monthBirthday = "August";
+        String yearBirthday = "1992";
+        String subjects = "Maths";
+        String hobbies = "Reading";
+        String picture = "1.png";
+        String picturePath = "img/1.png";
+        String address = "my currentAddress";
+        String state = "Haryana";
+        String city = "Panipat";
+        String stateAndCity = "Haryana Panipat";
+
+        step("Fill registration form", () -> {
+            practiceFormPage.openPage()
+                    .setFirstName(name)
+                    .setLastName(lastName)
+                    .setEmail(email)
+                    .setGender(gender)
+                    .setMobileNumber(mobileNumber)
+                    .setDateOfBirth(dayBirthday, monthBirthday, yearBirthday)
+                    .setSubjects(subjects)
+                    .setHobbies(hobbies)
+                    .setPicture(picturePath)
+                    .setCurrentAddress(address)
+                    .setState(state)
+                    .setCity(city)
+                    .submitClick();
+        });
+
+        step("Check registration form", () -> {
+            //Проверка вывода формы подтверждения
+            practiceFormPage.checkTitleOfResultPage("Thanks for submitting the form")
+                    .checkResult("Student Name", name + " " + lastName)
+                    .checkResult("Student Email", email)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", mobileNumber)
+                    .checkResult("Date of Birth", birthday)
+                    .checkResult("Subjects", subjects)
+                    .checkResult("Hobbies", hobbies)
+                    .checkResult("Picture", picture)
+                    .checkResult("Address", address)
+                    .checkResult("State and City", stateAndCity);
+        });
+
+        step("Close registration form", () -> {
+            //Клик Close
+            practiceFormPage.closeClick();
+        });
+
+    }
+}
