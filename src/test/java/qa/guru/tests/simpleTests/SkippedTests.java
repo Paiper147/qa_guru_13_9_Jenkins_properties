c9e8b94c23d3871c03770d1b138f39680cbdbf96
Ekaterina_Vasilevskaya <es-vasilevskaya@yandex.ru>
2022-07-02 12:45:34 +0700

    added allure-notifications

--- src/test/java/qa/guru/tests/simpleTests/SkippedTests.java
+++ src/test/java/qa/guru/tests/simpleTests/SkippedTests.java
@@ -1,10 +1,11 @@
 package qa.guru.tests.simpleTests;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-
+@Tag("simple")
 public class SkippedTests {
 
     @Disabled("причина")
--- src/test/java/qa/guru/tests/simpleTests/SuccessfulTests.java
+++ src/test/java/qa/guru/tests/simpleTests/SuccessfulTests.java
@@ -1,9 +1,10 @@
 package qa.guru.tests.simpleTests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+@Tag("simple")
 public class SuccessfulTests {
     @Test
     void test1(){
